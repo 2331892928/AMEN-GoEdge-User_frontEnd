9a3fbed336138ce07813a8f7f4a7c75608a22276
AMEN <amen@ymwl.edu.kg>
2022-11-30 21:40:42 +0800

    update 1.注册        2.登录        3.验证码        4.网站管理        5.添加/删除网站        6.https设置(http2/强制https/https证书/hsts/开关http|s/ocps/tls)        7.源站管理(支持端口/支持域名/支持ip/支持https/支持https证书/添加/删除)        8.证书管理(一键绑定/添加/删除)

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const timeStamp = new Date().getTime()
 
 module.exports = {
   lintOnSave: false,
-  // publicPath: process.env.NODE_ENV === 'production' ? '/vue-quasar-manage/' : '/',
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-quasar-manage/' : '/',
+  // publicPath: '/',
   devServer: {
     host: 'localhost',
     port: 8868,
